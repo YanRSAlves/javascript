a2d5865531e327abfdf181f878ec89e1c8bfb115
Yan Rocha Silva Alves <110752168+YanRSAlves@users.noreply.github.com>
2023-02-22 20:47:00 -0300

    button-like

--- clone play spotify/script.js
+++ clone play spotify/script.js
@@ -6,6 +6,7 @@ const cover = document.getElementById('cover');
 const play = document.getElementById('play');
 const next = document.getElementById('next');
 const previous = document.getElementById('previous');
+const likeButton = document.getElementById('like');
 const currentProgress = document.getElementById('current-progress');
 const progressContainer = document.getElementById('progress-container');
 const shuffleButton = document.getElementById('shuffle');
@@ -17,22 +18,26 @@ const totalTime = document.getElementById('total-time');
 const euMeRendo = {
     songName : 'Eu me Rendo',
     artist : 'Iguinho e Lulinha',
-    file: '0'                                   //Direcionamento Musica 1
+    file: '0' ,
+    liked: false,                                  //Direcionamento Musica 1
 }
 const Karolina = {
     songName : 'Karolina',
     artist : 'Wesley Safadão',
-    file: '1'                                   //Direcionamento Musica 2
+    file: '1' ,
+    liked: false,                                  //Direcionamento Musica 2
 }
 const UmPedido = {
     songName : 'Um Pedido',
     artist : 'Hungria',
-    file: '2'                                   //Direcionamento Musica 3
+    file: '2' ,
+    liked: false,                                   //Direcionamento Musica 3
 }
+
 let isPlaying = false;
 let isShuffled = false;
 let repeatOn = false;
-const originalplaylist = [euMeRendo, Karolina, UmPedido];    //Direcionamento Musicas
+const originalplaylist = JSON.parse(localStorage.getItem('playlist')) ?? [euMeRendo,Karolina,UmPedido];    //Direcionamento Musicas
 let sortedPlaylist = [...originalplaylist];
 let index = 0;
 
@@ -59,11 +64,26 @@ function playPauseDecider(){
     }
 
 }
+
+function likeButtonRender(){
+    if(sortedPlaylist[index].liked === true) {
+        likeButton.querySelector('.bi').classList.remove('bi-heart');
+        likeButton.querySelector('.bi').classList.add('bi-heart-fill');
+        likeButton.classList.add('button-active');
+    }else {
+        likeButton.querySelector('.bi').classList.add('bi-heart');
+        likeButton.querySelector('.bi').classList.remove('bi-heart-fill');
+        likeButton.classList.remove('button-active');
+    }
+
+}
+
 function InitializeSong(){
     cover.src = `imagens/${originalplaylist[index].file}.webp`;
     song.src = `songs/${originalplaylist[index].file}.mp3`;
     songName.innerText = originalplaylist[index].songName;
     bandName.innerText = originalplaylist[index].artist;
+    likeButtonRender();
 }
 function previousSong(){
     if (index === 0){
@@ -158,6 +178,17 @@ function updateTotalTime(){
     totalTime.innerText = toHHMMSS(song.duration); 
 }
 
+function likeButtonCliked(){
+    if(sortedPlaylist[index].liked === false){
+        sortedPlaylist[index].liked = true;
+    }
+    else {
+        sortedPlaylist[index].liked = false
+    }
+    likeButtonRender();
+    localStorage.setItem('playlist', JSON.stringify(sortedPlaylist));
+}
+
 
 // Execução de Função
 InitializeSong();
@@ -172,3 +203,4 @@ song.addEventListener('loadedmetadata', updateTotalTime);
 progressContainer.addEventListener('click', jumpTo);
 shuffleButton.addEventListener('click', shuffleButtonClicked);
 repeatButton.addEventListener('click', repeatButtonClicked);
+likeButton.addEventListener('click', likeButtonCliked);
